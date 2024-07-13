98389d7bbc58413d6fb39be541700aa8f8f81db7
Syswraith <77088155+syswraith@users.noreply.github.com>
2024-07-13 23:58:42 +0530

    Minimized bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,5 @@
-example=[8,10,15,12,45,65,23,254,545,2313,132,13];swap=True
-while swap:
-    swap=False 
-    for i in range(len(example)-1):
-        if example[i]>example[i+1]:swap=True;example[i],example[i+1]=example[i+1],example[i];print(example)
+e=[8,10,15,12,45,65,23,254,545,2313,132,13];s=1
+while s:
+    s=0 
+    for i in range(len(e)-1):
+        if e[i]>e[i+1]:s=1;e[i],e[i+1]=e[i+1],e[i];print(e)
