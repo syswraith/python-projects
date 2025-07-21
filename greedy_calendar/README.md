e5ee7c47e6ae0524401f798723b5845edadb4f13
syswraith <syswraith@proton.me>
2025-07-21 21:03:02 +0530

    readme and comments

---
+++ greedy_calendar/README.md
@@ -0,0 +1,4 @@
+basically, you have to find the shortest path from one date to another
+you cannot go back in time (obv)
+
+implemented with Prim's algorithm
--- greedy_calendar/graph.py
+++ greedy_calendar/graph.py
@@ -1,14 +1,13 @@
-from rich.pretty import pprint as pprint
-
 number_of_days = 31
 graph = {}
 
 
 # Generate graph/days in the month
 for day in range(1, number_of_days):
-    for adjacent_days in range(day + 1, number_of_days + 1):  # +1 to include 31
+    for adjacent_days in range(day + 1, number_of_days + 1):
         distance = adjacent_days - day
         graph.setdefault(f"Day {day}", []).append((f"Day {adjacent_days}", distance))
+
 
 def greedy_path(at_day, goto_day):
     '''
@@ -16,7 +15,6 @@ def greedy_path(at_day, goto_day):
     Get the edges of at_day and sort them with respect to minimum distance from each other
     Get the first element of the sorted edges and go to that day
     Repeat the process
-
     '''
     if at_day > goto_day:
         print("Cannot go back to date")
@@ -24,11 +22,12 @@ def greedy_path(at_day, goto_day):
 
     current = at_day
     while current < goto_day:
-        edges = graph[f"Day {current}"]
-        edges.sort(key=lambda x: x[1])
-        next_day = int(edges[0][0].split()[1])
-        print(f"Day {current} -> ", end="")
-        current = next_day
+        edges = graph[f"Day {current}"] # Get all the edges
+        edges.sort(key=lambda x: x[1]) # Sort all the edges w.r.t their distance
+        next_day = int(edges[0][0].split()[1]) # Get the first day which has the most minimum distance, split and get its number (its initially a string)
+        print(f"Day {current} -> ", end="") # Print the current day
+        current = next_day # Set the current day to the next day
     print(f"Day {goto_day}")
+
 
 greedy_path(int(input('Current day: ')), int(input('Goto day: ')))
